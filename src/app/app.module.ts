4d84b525ef85a4e3da0834dee684997ffe8745eb
Austin Edwards <austintedwards@gmail.com>
2017-06-13 11:27:34 -0600

    favs as main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { Favorites } from '../pages/favorites/favorites';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    Favorites
   ],
   imports: [
     BrowserModule,
@@ -19,7 +19,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    Favorites
   ],
   providers: [
     StatusBar,
